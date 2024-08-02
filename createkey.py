4ae58774ec8feaf8551dc3bb3a068e8c34e94515
Danucosuko suko <71569318+Danucosukosuko@users.noreply.github.com>
2024-08-02 16:46:58 +0200

    Update createkey.py

--- createkey.py
+++ createkey.py
@@ -3,3 +3,4 @@ from cryptography.fernet import Fernet
 # Generar una nueva clave Fernet válida
 SECRET_KEY = Fernet.generate_key()
 print("Nueva clave Fernet generada:", SECRET_KEY)
+print("Crea un archivo key.iky con la clave pública en tu repositorio")
